b4f160292476062ba9660e58260efdd0da56bfe7
Itzel Ventura <venit1123@gmail.com>
2019-06-09 22:02:00 -0700

    Modified Nav import

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Nav.css'
+// import './App.css'
+
+class Nav extends  Component {
+    render() {
+        return (
+            
+
+            
+            <div>
+
+                <nav class="navbar navbar-dark bg-dark">
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/">About DIT</a>
+                        </li>
+                        <li >
+                            <a href="/">Contact Us</a>
+                        </li>
+                    </ul>
+                </nav>
+                
+
+                    
+
+            </div>
+        )
+    }
+}
+
+export default Nav;
