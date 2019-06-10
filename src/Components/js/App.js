46128d1990d61fb3610ff71244366e1656215c18
tonaramos <gtonaramos@gmail.com>
2019-06-10 00:11:26 -0700

    Incorporating routing to project.

--- src/Components/js/App.js
+++ src/Components/js/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 // import '../css/App.css';
-import Nav from './Nav'
+import Nav from './Nav';
+import Home from '../Home/Home';
+import About from '../About/About';
+import ContactUs from '../ContactUs/ContactUs';
+import Footer from '../Footer/Footer';
+
 
 function App() {
+  let routes = (
+    <Switch>
+      <Route path="/About" exact component={About} />
+      <Route path="/ContactUs" exact component={ContactUs} />
+      <Route path="/" exact component={Home} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <Nav></Nav>
+      {routes}
+      <Footer />
     </div>
   );
 }
