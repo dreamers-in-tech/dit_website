cc4eb987195e6d24db6bd19079210ee918258deb
Itzel Ventura <venit1123@gmail.com>
2019-06-09 22:04:04 -0700

    Created new directories

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
