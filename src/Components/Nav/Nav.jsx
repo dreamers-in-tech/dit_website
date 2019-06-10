90cccf027c3eb6fb827cd94f0aca52b6d57917f4
Itzel Ventura <venit1123@gmail.com>
2019-06-10 09:20:32 -0700

    Added logo to the nav bar and moved links to the left

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,32 +1,26 @@
 import React, { Component } from 'react';
 import './Nav.css'
-// import './App.css'
+import './Nav.css'
+import logo from '../../assets/images/colorLogoTransparentBackground.png'
 
 class Nav extends  Component {
     render() {
         return (
-            
-
-            
             <div>
-
-                <nav class="navbar navbar-dark bg-dark">
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
+                <nav class="navbar navbar-light justify-content-end">
+                    <img  src={logo} width="65" height="65" alt="dreamers in tech logo"></img>
+                    <ul class="nav" >
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Home</a>
                         </li>
-                        <li>
-                            <a href="/">About DIT</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">About DIT</a>
                         </li>
-                        <li >
-                            <a href="/">Contact Us</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Contact Us</a>
                         </li>
                     </ul>
                 </nav>
-                
-
-                    
-
             </div>
         )
     }
