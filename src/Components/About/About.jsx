297b82fb43aeca6be9741c85fbe39af29cd1db05
tonaramos <gtonaramos@gmail.com>
2019-06-10 12:22:55 -0700

    Integrating UI files to main components

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import MemberGrid from '../UI/MemberGrid/MemberGrid';
 
 function About() {
   return (
-    <div class="bg-primary">
-      About
+    <div class="bg-info">
+      <div>
+        Mission
+      </div>
+      <div class="bg-primary">
+        Our Story
+      </div>
+      <div class="bg-info">
+        <div>
+          Who are we
+        </div>
+        <MemberGrid />
+      </div>
     </div>
   );
 }
