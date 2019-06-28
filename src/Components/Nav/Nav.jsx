a7010385be5ebd2a254c412bf60a57293288ef96
Itzel Ventura <venit1123@gmail.com>
2019-06-28 12:44:03 -0700

    Added routes to nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,32 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Nav.css'
 // import './App.css'
 
 class Nav extends  Component {
     render() {
         return (
-            
-
-            
             <div>
-
                 <nav class="navbar navbar-dark bg-dark">
                     <ul>
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/">About DIT</a>
-                        </li>
-                        <li >
-                            <a href="/">Contact Us</a>
-                        </li>
+                        <Link to="home" class="">
+                            Home
+                        </Link>
+                    </ul>
+                    <ul>
+                        <Link to="about" class="">
+                            About
+                        </Link>
+                    </ul>
+                    <ul>
+                        <Link to="contact" class="">
+                            Contact
+                        </Link>
                     </ul>
                 </nav>
-                
-
-                    
-
             </div>
         )
     }
